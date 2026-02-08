4a8f41ef312a2262a6ee562eda2ae30f459f8419
Matt Goldberg <matt.q.goldberg@gmail.com>
2026-02-07 19:50:33 -0800

    Updated http server skeleton; now handles too large request and will give a proper hello in HTTP format

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
 import socket
+from enum import Enum
 
 HOST = "127.0.0.1"
 PORT = 8080
 BUFSIZE = 1024
+MAX_REQ_SIZE = 8 * 1024
+
+class RequestStatus(Enum):
+    IN_PROGRESS = 1
+    COMPLETE = 2
+    ABORTED = 3
+    TOO_LARGE = 4
 
 # Appends data to request bytearray until end of headers is found
-def build_request(request: bytearray, data: bytes):
+def build_request(request: bytearray, data: bytes, conn: socket.socket):
+
+    if data == b"":
+        return RequestStatus.ABORTED
 
     request.extend(data)
 
+    if len(request) > MAX_REQ_SIZE:
+        if not send_response(conn, b"HTTP/1.0 413 Payload Too Large\r\nContent-Length: 0\r\nConnection: close\r\n\r\n"):
+            return RequestStatus.ABORTED
+        return RequestStatus.TOO_LARGE
+
     if b'\r\n\r\n' in request:
-        print('Request headers complete')
-        print(request)
-        return True
+        headers, body = request.split(b'\r\n\r\n', 1)
+        return handle_request(headers, body, conn)
 
-    return False
+    return RequestStatus.IN_PROGRESS
 
-def handle_request(request, conn: socket.socket):
+def handle_request(headers: bytearray, body: bytearray, conn: socket.socket):
     print("Handling request")
-    conn.sendall(b"Hello\n")
-    conn.close()
+    if not send_response(conn, b"HTTP/1.0 200 OK\r\nContent-Length: 6\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nHello\n"):
+        return RequestStatus.ABORTED
+
+    return RequestStatus.COMPLETE
+
+def send_response(conn: socket.socket, response: bytes):
+    try:
+        conn.sendall(response)
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def handle_connection(s: socket.socket):
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+
+        # Once connected, receive and respond to requests
+        request = bytearray()
+        request_status = RequestStatus.IN_PROGRESS
+        while request_status == RequestStatus.IN_PROGRESS:
+            try:
+                data = conn.recv(BUFSIZE)
+                print("Data recieved")
+                request_status = build_request(request, data, conn)
+            except:
+                request_status = RequestStatus.ABORTED
+
+        print("Request handled:", request_status.name)
+
+    print("Connection closed")
+
+
 
 def main():
     print("Starting server")
@@ -28,26 +75,9 @@ def main():
         s.bind((HOST, PORT))
         s.listen(1)
 
-        # Keep connection listen for new connections indefinitely
+        # Keep listening for new connections indefinitely
         while(True):
-            conn, addr = s.accept()
-            print('Connected by', addr)
-
-            # Once connected, receive and respond to requests
-            request = bytearray()
-            request_complete = False
-            while not request_complete:
-                try:
-                    data = conn.recv(BUFSIZE)
-                    print("Data recieved")
-                    request_complete = build_request(request, data)
-                except:
-                    conn.close()
-                    break
-
-            if(request_complete):
-                handle_request(request, conn)
-
+            handle_connection(s)
 
 
 if __name__ == "__main__":
